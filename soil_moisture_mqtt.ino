189dfadab28bdb0c5dc109f69fd411d752ffdc25
Sannu239 <57701894+Sannu239@users.noreply.github.com>
2020-02-24 17:47:43 +0530

    Update soil_moisture_mqtt.ino

--- soil_moisture_mqtt.ino
+++ soil_moisture_mqtt.ino
@@ -7,7 +7,7 @@ int red_LED = D3; //    LED IF NEED OF WATER
 int thresholdValue =800; 
 //char d11[32];
 char s22[32];
-sanu
+#i made chage in git hub
 
 //===================pass and ssid========================================================================================
 const char* ssid = "satyam";
